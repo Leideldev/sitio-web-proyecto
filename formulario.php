1ea621869b83d5241231b230535da013db926305
JoshelJavv <55430787+JoshelJavv@users.noreply.github.com>
2020-06-02 17:07:44 -0500

    Se agregó validación de los campos

--- formulario.php
+++ formulario.php
@@ -64,14 +64,14 @@ if(isset($_SESSION['nombre_usuario']))
   </nav>
     <form action="./controlador/controlador_usuario.php" method="POST">
         <h2>Registro</h2>
-        <input type="text" class="form-control" placeholder="Nombre" name="nombre">
-        <input type="text" class="form-control" placeholder="Apellido" name="apellido">
+        <input type="text" class="form-control" placeholder="Nombre" name="nombre" required pattern="[a-zA-Z]*" oninput="this.setCustomValidity('')" oninvalid="this.setCustomValidity('Escribe un Nombre válido. Solo Letras, no números ni caracteres especiales')">
+        <input type="text" class="form-control" placeholder="Apellido" name="apellido" required required pattern="[a-zA-Z]*" oninput="this.setCustomValidity('')" oninvalid="this.setCustomValidity('Escribe un Apellido válido. Solo Letras, no números ni caracteres especiales')">
         Usuario
-        <input type="radio"  placeholder="Nombre" name="tipo_usuario" value="usuario">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="radio"  placeholder="Nombre" name="tipo_usuario" value="usuario" required>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         Publicista
-        <input type="radio"  placeholder="Nombre" name="tipo_usuario" value="publicista">
-        <input type="text" class="form-control" placeholder="Nombre de usuario" name="nombre_usuario">
-        <input type="password" class="form-control" placeholder="Contrase&ntilde;a" name="contrasena">
+        <input type="radio"  placeholder="Nombre" name="tipo_usuario" value="publicista" required>
+        <input type="text" class="form-control" placeholder="Nombre de usuario" name="nombre_usuario" required>
+        <input type="password" class="form-control" placeholder="Contrase&ntilde;a" name="contrasena" required>
         <input type="submit" class="btn btn-primary"></input>
         <input type="hidden" id="mandar" name="mandar" value="registroUsuario">
     </form>
